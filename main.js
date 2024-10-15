e4ddf15dce0e2174657bb62055d4e05033729023
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-10-15 21:50:37 +0200

    PRINTING HELLOW WORLD

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+console.log("Hello, World!");
+
+
+/*
+PRACTICE
 let x =2;
 console.log(x)
+let age =38;
+console.log(age)
+let FirstName ="YOUSSEF";
+console.log(`my name is ${FirstName}`);
+console.log("Hello,Welcom world");*/
