64a3743b7b617cdce0dd60bac8b8fb771ba516e0
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-10-15 22:00:29 +0200

    Basic arithmetic operations

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ let color = "Blue";
 
 console.log(`My name is ${name}, I am ${age} years old and my favorite color is ${color}`);
 
+let invoice1 = 190;
+let invoice2 = 200;
+total = invoice1 + invoice2
+let halfTotal = total / 2;
+let duobleTotal = total * 2;
+let diffrentInvoice = invoice1 - invoice2;
+console.log(`The total of the two invoices is ${total}`);
+console.log(`The half of the total is ${halfTotal}`);
+console.log(`The double of the total is ${duobleTotal}`);
+console.log(total);
+
 
 /*
 PRACTICE
